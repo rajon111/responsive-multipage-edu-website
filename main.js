9b1d8bfbb2f29069bbd12ae2d68f2c77166e4f7e
rajon <mostofarajon111@gmail.com>
2022-03-12 23:10:45 +0600

    testomonials part added

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
+//show & hide faq answer
+const faqs = document.querySelectorAll('.faq')
+// console.log(faqs)
+faqs.forEach(faq =>{
+    faq.addEventListener('click', ()=>{
+        faq.classList.toggle('open')
+        //change icon
+        /*const icon = document.querySelector('.faq__icon i');
+        if(icon.classList === 'fa-light fa-plus'){
+            icon.classList ='fa-light fa-minus'
+        }else{
+            icon.classList ='fa-light fa-plus'
+        }*/
+    })
+})
