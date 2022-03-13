4c9cf04ba92645db4f5d174d307ad598e045bc90
rajon <mostofarajon111@gmail.com>
2022-03-13 17:19:49 +0600

    drop-down navbar added

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ faqs.forEach(faq =>{
         }*/
     })
 })
+//show nave meanu
+const menu = document.querySelector('.nav__menu')
+const menuBtn = document.querySelector('#open-menu-btn')
+const closeBtn = document.querySelector('#close-menu-btn')
+
+menuBtn.addEventListener('click', ()=>{
+    menu.style.display ='flex'
+    closeBtn.style.display='inline-block'
+    menuBtn.style.display='none'
+})
+
+//close nav
+closeBtn.addEventListener('click', ()=>{
+    menu.style.display ='none'
+    closeBtn.style.display='none'
+    menuBtn.style.display='inline-block'
+})
